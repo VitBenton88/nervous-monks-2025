7abc0a4fb09c763320ca21038ece1397c1c1ef80
Vit Benton <Vit@vitbenton.com>
2025-02-26 18:02:12 -0500

    yt embed prop fixes

--- src/components/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed.tsx
@@ -11,10 +11,9 @@ const YoutubeEmbed: React.FC<YoutubeEmbedProps> = ({ embedId = "9XjjFLAchb0" }):
             height="315" 
             src={`https://www.youtube.com/embed/${embedId}`}
             title="YouTube video player"
-            frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" 
-            allowfullscreen
+            referrerPolicy="strict-origin-when-cross-origin" 
+            allowFullScreen
         >
         </iframe>
     )
